44e30dae6bcc65ed714ceac6fc7dc623aba0e8be
Dawinia Lo <dawinialo@163.com>
2019-07-18 09:30:22 +0800

    15, 16两道题

--- Array/15.3Sum.cpp
+++ Array/15.3Sum.cpp
@@ -9,59 +9,61 @@
 #include <queue>
 
 using namespace std;
-
-vector<vector<int>> threeSum(vector<int> &nums)
+class Solution
 {
-    vector<vector<int>> result;
-    if (nums.size() < 3)
-        return result;
-    sort(nums.begin(), nums.end());
-    for (int i = 0; i < nums.size() - 2; ++i)
+  public:
+    vector<vector<int>> threeSum(vector<int> &nums)
     {
-        int target = -nums[i];
-        if (target < 0)
-            break;
-        int l = i + 1;
-        int r = nums.size() - 1;
-        if (nums[i] + nums[l] > 0)
-            break;
-        while (l < r)
+        vector<vector<int>> result;
+        if (nums.size() < 3)
+            return result;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 2; ++i)
         {
-            int sum = nums[l] + nums[r];
-            if (target == sum)
+            int target = -nums[i];
+            if (target < 0)
+                break;
+            int l = i + 1;
+            int r = nums.size() - 1;
+            if (nums[i] + nums[l] > 0)
+                break;
+            while (l < r)
             {
-                vector<int> oneRes = {nums[i], nums[l], nums[r]};
-                result.push_back(oneRes);
-                while (l < r && nums[l] == nums[l + 1])
+                int sum = nums[l] + nums[r];
+                if (target == sum)
+                {
+                    vector<int> oneRes = {nums[i], nums[l], nums[r]};
+                    result.push_back(oneRes);
+                    while (l < r && nums[l] == nums[l + 1])
+                        l++;
+                    while (l < r && nums[r] == nums[r - 1])
+                        r--;
                     l++;
-                while (l < r && nums[r] == nums[r - 1])
                     r--;
-                l++;
-                r--;
+                } else if (target < sum)
+                {
+                    r--;
+                } else
+                {
+                    l++;
+                }
             }
-            else if (target < sum)
-            {
-                r--;
-            }
-            else
-            {
-                l++;
-            }
+            while (i + 1 < nums.size() && nums[i + 1] == nums[i])
+                i++;
         }
-        while (i + 1 < nums.size() && nums[i + 1] == nums[i])
-            i++;
+        return result;
     }
-    return result;
-}
+};
 
 int main()
 {
+    Solution solution;
     vector<int> nums = {1, -1, -1, 0};
-    for (int i = 0; i < threeSum(nums).size(); ++i)
+    for (int i = 0; i < solution.threeSum(nums).size(); ++i)
     {
-        for (int j = 0; j < threeSum(nums)[i].size(); ++j)
+        for (int j = 0; j < solution.threeSum(nums)[i].size(); ++j)
         {
-            cout << threeSum(nums)[i][j] << " ";
+            cout << solution.threeSum(nums)[i][j] << " ";
         }
         cout << endl;
     }
--- Array/16.3SumClosest.cpp
+++ Array/16.3SumClosest.cpp
@@ -10,17 +10,43 @@
 
 using namespace std;
 
-int threeSumClosest(vector<int> &nums, int target)
+class Solution
 {
-    int result = target;
-    return result;
-}
+  public:
+    int threeSumClosest(vector<int> &nums, int target)
+    {
+        if (nums.size() < 3)
+            return 0;
+        sort(nums.begin(), nums.end());
+        int minTarget = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.size() - 2; ++i)
+        {
+            int l = i + 1;
+            int r = nums.size() - 1;
+            while (l < r)
+            {
+                int sum = nums[l] + nums[r] + nums[i];
+                minTarget = abs(minTarget - target) > abs(sum - target) ? sum : minTarget;
+                if (sum == target)
+                    return target;
+                else if (sum > target)
+                    --r;
+                else
+                    ++l;
+            }
+            while (i + 1 < nums.size() && nums[i + 1] == nums[i])
+                ++i;
+        }
+        return minTarget;
+    }
+};
 
 int main()
 {
-    vector<int> nums = {-1, 2, 1, -4};
+    Solution solution;
+    vector<int> nums = {0, 2, 1, -3};
     int target = 1;
-    cout << threeSumClosest(nums, target) << endl;
+    cout << solution.threeSumClosest(nums, target) << endl;
     return 0;
 }
 
