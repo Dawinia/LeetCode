0842b19ac4a46c48b5a1d8f2559f1361c0d6a623
Dawinia Lo <dawinialo@163.com>
2020-04-24 18:23:47 +0800

    2020-4-24 dp

---
+++ DynamicProgramming/面试题 08.11. 硬币.py
@@ -0,0 +1,10 @@
+class Solution:
+    def waysToChange(self, n: int) -> int:
+        coins = [1, 5, 10, 25]
+        dp = [0] * (n + 1) 
+        dp[0] = 1
+        for coin in coins :
+            for i in range(coin, n + 1) :
+                dp[i] = (dp[i] + dp[i - coin])
+
+        return dp[n] % 1000000007
